cac2c120bf9dd46ba081218e8d4c6a698ca9a7e4
AUMGSOEMOE <soemoe2580aa@gmail.com>
2024-03-27 15:02:32 +0630

    error_page

---
+++ src/app/product/[productId]/reviews/[reviewId]/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+    return <>
+        <h1>page doesn't exist</h1>
+        <p>something</p>
+    </>
+
+}
--- src/app/product/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/product/[productId]/reviews/[reviewId]/page.tsx
@@ -1,3 +1,8 @@
+import {notFound} from "next/navigation"
+
 export default function Product({params}:{params:{reviewId:string,productId : string}})  {
+    if(parseInt(params.reviewId) > 100){
+        notFound()
+    }
     return <h1>{params.reviewId} reviews of {params.productId} product</h1>;
 }
